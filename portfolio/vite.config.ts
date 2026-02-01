60357aceafec10cc9d133062d5916e79fce79750
phway-123 <phway04.23@ichat.sp.edu.sg>
2026-02-01 19:55:05 +0800

    commit

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,23 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
-
+// https://vite.dev/config/
 // export default defineConfig({
-//   base: '/phwayphway/',
 //   plugins: [react()],
 // })
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
+  base: '/phwayphway/',
   plugins: [react()],
-  build: {
-    outDir: "./", // Build directly to repo root
-    emptyOutDir: false // Don't delete existing files
-  }
-});
+})
